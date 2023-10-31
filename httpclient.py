c66c155ebf7ffea9a5613738c913834deadbc1e1
itc-n23023 <n23023@std.it-college.ac.jp>
2023-10-31 08:30:33 +0000

    10/31-4

--- httpclient.py
+++ httpclient.py
@@ -2,16 +2,14 @@
 
 import socket
 
-
 def send_msg(sock, msg):
     total_sent_len = 0
     total_msg_len = len(msg)
     while total_sent_len < total_msg_len:
-        sent_len = sock.send(msg[total_sent_len:])
+        sent_len= sock.send(msg[total_sent_len:])
         if sent_len == 0:
             raise RuntimeError('socket connection broken')
         total_sent_len += sent_len
-
 
 def recv_msg(sock, chunk_len=1024):
     while True:
@@ -20,10 +18,10 @@ def recv_msg(sock, chunk_len=1024):
             break
         yield received_chunk
 
-
 def main():
+    ip_address = input()
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('127.0.0.1', 80))
+    client_socket.connect((ip_address, 80))
     request_text = 'GET / HTTP/1.0\r\n\r\n'
     request_bytes = request_text.encode('ASCII')
     send_msg(client_socket, request_bytes)
@@ -32,6 +30,6 @@ def main():
     print(received_text)
     client_socket.close()
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
+	
